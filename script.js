b6504e52c9c9f97183c5ef1734fb318a26864f3b
Randira <204060028.mhs@stmikjabar.ac.id>
2023-05-06 19:06:20 +0700

    update file js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 // Animasi Scroll To Top
 const scrollToTopBtn = document.querySelector("#scroll-to-top-btn");
 
-window.addEventListener("scroll", () => {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    scrollToTopBtn.style.display = "block";
-  } else {
-    scrollToTopBtn.style.display = "none";
-  }
-});
+if (scrollToTopBtn) {
+  window.addEventListener("scroll", () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  });
 
-scrollToTopBtn.addEventListener("click", () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-});
+  scrollToTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+}
 
 // Efek Parallax
 const homeHero = document.querySelector(".home-hero");
